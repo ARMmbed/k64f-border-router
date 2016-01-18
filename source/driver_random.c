30c46422909174b1daa12f53fda56c11ef00c8b5
Jani Pellikka <jani.pellikka@arm.com>
2016-01-18 16:10:55 +0200

    Initial directory structure.

---
+++ source/driver_random.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2015-2015, ARM Limited, All Rights Reserved
+ */
+
+#include "ns_types.h"
+
+void arm_random_module_init(void)
+{
+}
+
+uint32_t arm_random_seed_get(void)
+{
+	return 0;
+}
